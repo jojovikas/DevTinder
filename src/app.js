97a6fe259c1b77bf450e0fd7d60a022182881247
jojovikas <vikasfrontenddeveloper007@gmail.com>
2024-11-10 17:18:29 +0530

    hello

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const User = require("./models/user");
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  // console.log(req.body);
+  console.log(req.body);
 
   // creating a new instance of the user model
   const user = new User(req.body);
@@ -18,6 +18,33 @@ app.post("/signup", async (req, res) => {
     res.send("User Added Successfully");
   } catch (err) {
     res.status(400).send("Error Saving the user :", +err.message);
+  }
+});
+
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.eamilId;
+  // console.log(userEmail);
+
+  try {
+    const users = await User.find({ eamilId: userEmail });
+
+    if (users.length === 0) {
+      res.status(404).send("user not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  // Fetch All users data
+  try {
+    const users = await dUser.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("something went Wrong");
   }
 });
 
